6c67d8344f4a4ef8da20fa38375ba598ecb7ed6a
李梦龙 <649076408@qq.com>
2020-07-28 14:34:11 +0800

    docs($router): add vue-router next

--- src/App.vue
+++ src/App.vue
@@ -3,95 +3,21 @@
     alt="Vue logo"
     src="./assets/logo.png"
   >
-  <w-text>Water</w-text>
-  <w-text strong>
-    Water
-  </w-text>
-  <w-text underline>
-    Water
-  </w-text>
-  <w-text
-    underline
-    strong
-  >
-    Water
-  </w-text>
-  <w-text
-    underline
-    strong
-    code
-  >
-    Water
-  </w-text>
-  <w-text
-    underline
-    strong
-    mark
-  >
-    Water
-  </w-text>
-  <w-text
-    underline
-    strong
-    mark
-    mark-style="background-color: #ff4949;color:#fff;padding: 0 4px;"
-  >
-    Water
-  </w-text>
-  <w-text disabled>
-    Water
-  </w-text>
-  <w-text delete>
-    Water
-  </w-text>
-  <w-text>Water</w-text>
-  <w-text type="secondary">
-    Water
-  </w-text>
-  <w-text type="warning">
-    Water
-  </w-text>
-  <w-text type="info">
-    Water
-  </w-text>
-  <w-text type="danger">
-    Water
-  </w-text>
-  <w-text type="success">
-    Water
-  </w-text>
-  <w-text
-    underline
-    strong
-    code
-    mark
-    mark-style="background-color: #ff4949;color:#0ff;padding: 0 4px;"
-  >
-    Water
-  </w-text>
-  <w-text
-    underline
-    underline-style="color:#ff4949;"
-  >
-    Water
-  </w-text>
-  <w-text
-    delete
-    delete-style="color:#ff4949;"
-  >
-    Water
-  </w-text>
+  <br>
+  <router-link :to="{ name: 'Home'}">
+    首页
+  </router-link> |
+  <router-link :to="{ name: 'Text'}">
+    文字
+  </router-link>
+  <br>
+  <router-view />
 </template>
 
 <script>
 /** @format */
 
-import WText from './components/text/Text.vue';
-
 export default {
   name: 'App',
-  components: {
-    WText,
-  },
 };
 </script>
---
+++ src/views/Text.vue
@@ -0,0 +1,92 @@
+<template>
+  <w-text>Water</w-text>
+  <w-text strong>
+    Water
+  </w-text>
+  <w-text underline>
+    Water
+  </w-text>
+  <w-text
+    underline
+    strong
+  >
+    Water
+  </w-text>
+  <w-text
+    underline
+    strong
+    code
+  >
+    Water
+  </w-text>
+  <w-text
+    underline
+    strong
+    mark
+  >
+    Water
+  </w-text>
+  <w-text
+    underline
+    strong
+    mark
+    mark-style="background-color: #ff4949;color:#fff;padding: 0 4px;"
+  >
+    Water
+  </w-text>
+  <w-text disabled>
+    Water
+  </w-text>
+  <w-text delete>
+    Water
+  </w-text>
+  <w-text>Water</w-text>
+  <w-text type="secondary">
+    Water
+  </w-text>
+  <w-text type="warning">
+    Water
+  </w-text>
+  <w-text type="info">
+    Water
+  </w-text>
+  <w-text type="danger">
+    Water
+  </w-text>
+  <w-text type="success">
+    Water
+  </w-text>
+  <w-text
+    underline
+    strong
+    code
+    mark
+    mark-style="background-color: #ff4949;color:#0ff;padding: 0 4px;"
+  >
+    Water
+  </w-text>
+  <w-text
+    underline
+    underline-style="color:#ff4949;"
+  >
+    Water
+  </w-text>
+  <w-text
+    delete
+    delete-style="color:#ff4949;"
+  >
+    Water
+  </w-text>
+</template>
+
+<script>
+/** @format */
+
+import WText from '../components/text/Text.vue';
+
+export default {
+  components: {
+    WText,
+  },
+};
+</script>
